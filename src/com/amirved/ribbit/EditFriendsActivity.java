e52eb9b6372d802ded8552a7e107162891dca355
Amir Vedadi <amir.ved@gmail.com>
2014-06-23 17:56:45 +0200

    Ribbit noDestruct (LogoutBug)

--- src/com/amirved/ribbit/EditFriendsActivity.java
+++ src/com/amirved/ribbit/EditFriendsActivity.java
@@ -7,17 +7,23 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
+import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class EditFriendsActivity extends ListActivity {
+	
+	protected ParseRelation<ParseUser> mFriendsRelation;
+	protected ParseUser mCurrentUser;
 	
 	public static final String TAG = EditFriendsActivity.class.getSimpleName();
 	
@@ -30,11 +36,16 @@ public class EditFriendsActivity extends ListActivity {
 		setContentView(R.layout.activity_edit_friends);
 		// Show the Up button in the action bar.
 		setupActionBar();
+		
+		getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
 	}
 	
 	@Override
 	protected void onResume() {
 		super.onResume();
+		
+		mCurrentUser = ParseUser.getCurrentUser();
+		mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
 		
 		setProgressBarIndeterminateVisibility(true);
 		
@@ -60,6 +71,8 @@ public class EditFriendsActivity extends ListActivity {
 							android.R.layout.simple_list_item_checked,
 							usernames);
 					setListAdapter(adapter);
+					
+					addFriendCheckmarks();
 				}
 				else {
 					Log.e(TAG, e.getMessage());
@@ -84,13 +97,6 @@ public class EditFriendsActivity extends ListActivity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.edit_friends, menu);
-		return true;
-	}
-
-	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
@@ -107,4 +113,59 @@ public class EditFriendsActivity extends ListActivity {
 		return super.onOptionsItemSelected(item);
 	}
 
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
+		
+		if (getListView().isItemChecked(position)) {
+			// add the friend
+			mFriendsRelation.add(mUsers.get(position));
+		}
+		else {
+			// remove the friend
+			mFriendsRelation.remove(mUsers.get(position));
+		}
+
+		mCurrentUser.saveInBackground(new SaveCallback() {
+			@Override
+			public void done(ParseException e) {
+				if (e != null) {
+					Log.e(TAG, e.getMessage());
+				}
+			}
+		});
+	}
+	
+	private void addFriendCheckmarks() {
+		mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
+			@Override
+			public void done(List<ParseUser> friends, ParseException e) {
+				if (e == null) {
+					// list returned - look for a match
+					for (int i = 0; i < mUsers.size(); i++) {
+						ParseUser user = mUsers.get(i);
+						
+						for (ParseUser friend : friends) {
+							if (friend.getObjectId().equals(user.getObjectId())) {
+								getListView().setItemChecked(i, true);
+							}
+						}
+					}
+				}
+				else {
+					Log.e(TAG, e.getMessage());
+				}
+			}
+		});
+	}
 }
+
+
+
+
+
+
+
+
+
+
